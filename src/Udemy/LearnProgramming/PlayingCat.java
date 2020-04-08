8dadaf7d613188b0d7e9b731b26f3a079d943fc7
SasikalaRajiv <sasifelicia@outlook.com>
2020-04-08 18:32:13 +0530

    @override_review Udemy_Exercise 17 - PlayingCat

---
+++ src/Udemy/LearnProgramming/PlayingCat.java
@@ -0,0 +1,16 @@
+package Udemy.LearnProgramming;
+
+public class PlayingCat {
+    public static void main(String[] args){
+        System.out.println(isCatPlaying(true,46));
+    }
+    public static boolean isCatPlaying(boolean summer, int temp){
+        boolean isPlaying =false;
+         if((summer==true)&&(temp>25&&temp<=45)){
+              isPlaying = true;
+        } else if (temp>25 && temp <=35) {
+             isPlaying=true;
+         }
+         return isPlaying;
+    }
+}
